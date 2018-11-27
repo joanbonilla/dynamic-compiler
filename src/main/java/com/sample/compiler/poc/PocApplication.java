9b64bc6d07058eb61884a79076a9b4ca8999ba5e
juanmiguel.bonilla <juanmiguel.bonilla@altran.com>
2018-11-27 15:07:23 +0100

    ADD spring boot sample with sample

---
+++ src/main/java/com/sample/compiler/poc/PocApplication.java
@@ -0,0 +1,31 @@
+package com.sample.compiler.poc;
+
+import org.joor.Reflect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.function.Supplier;
+
+@SpringBootApplication
+public class PocApplication {
+
+	public static void main(String[] args) {
+
+		Supplier<String> supplier = Reflect.compile(
+				"com.sample.compiler.poc.RuntimeCompilerTest",
+				"package com.sample.compiler.poc; " +
+						"import com.sample.compiler.poc.bo.SampleBO; " +
+						"class RuntimeCompilerTest implements java.util.function.Supplier<String> { " +
+						"public String get() { " +
+						"return \"Hello World!\"; } " +
+						"}"
+		).create().get();
+
+		System.out.println("----------------------------- REFLECT -----------------------------");
+		System.out.println(supplier.getClass() + " - " + supplier.get());
+		System.out.println("-------------------------------------------------------------------");
+
+		SpringApplication.run(PocApplication.class, args);
+	}
+}
---
+++ src/main/java/com/sample/compiler/poc/bo/SampleBO.java
@@ -0,0 +1,17 @@
+package com.sample.compiler.poc.bo;
+
+/**
+ * @author joan.bonilla
+ */
+public class SampleBO {
+
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
